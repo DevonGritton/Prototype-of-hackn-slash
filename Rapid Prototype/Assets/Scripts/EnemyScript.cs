e049491da7ab04ca6a6723bf4b4572ba2d5e16c6
DevonGritton <55791362+DevonGritton@users.noreply.github.com>
2020-01-28 13:11:57 +0000

    update
    a

---
+++ Rapid Prototype/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Hit");
+        if (other.gameObject.tag == "Player")
+        {
+            Object.Destroy(this.gameObject);
+        }
+    }
+}
